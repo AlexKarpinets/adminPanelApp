9138d2e05dfd0913511949e918990c6b9b7850e2
AlexKarpinets <66379289+AlexKarpinets@users.noreply.github.com>
2021-07-13 22:16:46 +0300

    Fix #4

--- adminPanelApp/LoginViewController.swift
+++ adminPanelApp/LoginViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
       
     }
 
-
+    @IBAction func loginButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func forgotUNButton(_ sender: UIButton) {
+        showAlert(title: "Le pardon!😎", message: "Your Name is - 'Name'!")
+    }
+    
+    @IBAction func forgotPasswordButton(_ sender: UIButton) {
+        
+    }
+  
 }
 
+extension LoginViewController {
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "HOROSHO", style: .default) { _ in
+            self.nameTextField.text = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+  
+}
