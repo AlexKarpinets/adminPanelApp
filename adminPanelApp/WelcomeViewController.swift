b8ebd99b9cdd5de1faf88eec8f2e70b1d226c0d8
AlexKarpinets <66379289+AlexKarpinets@users.noreply.github.com>
2021-07-13 21:17:02 +0300

    Add WelcomeViewController

---
+++ adminPanelApp/WelcomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeViewController.swift
+//  adminPanelApp
+//
+//  Created by Karpinets Alexander on 13.07.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
